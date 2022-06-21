aa9f59170eef278d7714899a17ec7d1a1be651ce
Trog19 <tsrogers8@msn.com>
2022-06-21 13:34:32 -0600

    Conncted frint end to backend through axios and confirmed data is being shown on React App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 
 
 function App() {
+  const [songs, setSongs] = useState([]);
+
+  useEffect(()=>{
+      GetAllSongs();
+  }, [])
+
+
+  async function GetAllSongs(){
+    console.log("Inside GetAllSongs function")
+    let response = await axios.get('http://127.0.0.1:8000/api/music/');
+    setSongs(response.data);
+    console.log("Songs data: ", response.data)
+  }
+
   return (
     <div >
-      <h3> hello world </h3>
     </div>
   );
+
+//   async function createSong(newSong){
+//     //newSong:
+//    // {
+//     //title: "Troubadour"
+//     //album: "Troubadour"
+//     //artist: "George Strait"
+//     //genre: "Country"
+//     //releaseDate: "04-01-2008"
+//  //   }
+//  let response = await axios.post('http://baseurl/api/music', newSong);
+//  if(response.status===201){
+//   await GetAllSongs
+//     }
+//   }
+
+
 }
 
 export default App;
--- src/components/DisplayMusic.jsx
+++ src/components/DisplayMusic.jsx
@@ -1,5 +1,7 @@
+import React from "react"
+import axios from 'Axios'
+
+
 const DisplayMusic= (props)=> {
-    return(
-        
-    )
+    return
 }
